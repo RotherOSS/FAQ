7adfeebbcdee578a9da844710200f54ec49941d8
Stefan Härter <sha@rother-oss.com>
2026-07-21 16:05:27 +0200

    Corrected POD for sub StateList().

--- Kernel/System/FAQ/State.pm
+++ Kernel/System/FAQ/State.pm
@@ -2,7 +2,7 @@
 # OTOBO is a web-based ticketing system for service organisations.
 # --
 # Copyright (C) 2001-2019 OTRS AG, https://otrs.com/
-# Copyright (C) 2019-2020 Rother OSS GmbH, https://otobo.de/
+# Copyright (C) 2019-2026 Rother OSS GmbH, https://otobo.io/
 # --
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -144,7 +144,7 @@ get the state list as hash
 
 optional, get state list for some state types:
 
-    my $StateTypeHashRef = $FAQObject->StateTypeList(
+    my %StateTypeHash = $FAQObject->StateTypeList(
         Types  => [ 'public', 'internal'],
         UserID => 1,
     );
